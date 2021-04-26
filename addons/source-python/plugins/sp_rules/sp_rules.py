5c5f3d7fa10214ed486db88e2487322e18a181e8
srpg <44681214+srpg@users.noreply.github.com>
2021-04-26 22:24:48 +0300

    Improved code shorter

--- addons/source-python/plugins/sp_rules/sp_rules.py
+++ addons/source-python/plugins/sp_rules/sp_rules.py
@@ -1,85 +1,38 @@
-#========================
-# Imports
-#========================
 from engines.server import queue_command_string
+from players.entity import Player
 from players.helpers import index_from_userid, userid_from_index
 from events import Event
-from listeners.tick import Delay
 from menus import SimpleMenu, SimpleOption, Text
+from messages import SayText2
 
-#========================
-# Config
-#========================
-
-get_rules = ['No Spawn Killing', 'No Flaming', 'No Chat/Mic Spam', 'No Bomb Griefing', 'No Other Servers Adverting', 'No Bug Abuse', 'No Suicide'] # Rules text
-
-#========================
-# Server Command
-#========================
-
-def server_command(__cmd__):
-	queue_command_string(__cmd__)
-
-#========================
-# Userid
-#========================
-
-def indexFromUserid(userid):
-	return index_from_userid(userid)
-
-def useridFromIndex(index):
-	return userid_from_index(index)
-
-#========================
-# Menu
-#========================
-
-def is_queued(_menu, _index):
-	q = _menu._get_queue_holder()
-	for i in q:
-		if i == _index:
-			for x in q[i]:
-				return True
-	return False
-
-#========================
-# Menu Callback
-#========================
-
-def rule_menu_callback(_menu, _index, _option):
-	choice = _option.value
-	if choice:
-		userid = useridFromIndex(_index)        
-		if choice == 'No':
-			server_command('kickid %s You have to accept the rules!' % (userid))    
-        
-#========================
-# Event
-#========================
+get_rules = ['No Spawn Killing', 'No Flaming', 'No Chat/Mic Spam', 'No Bomb Griefing', 'No Other Servers Adverting', 'No Bug Abuse', 'No Suicide'] # The rules text for menu
 
 @Event('player_activate')
 def player_activate(args):
-	userid = args.get_int('userid')   
-	Delay(2.0, rules, (userid,))
-
-#========================
-# Rules Menu
-#========================
+	userid = args.get_int('userid')
+	Player(index_from_userid(userid)).delay(2.0, rules, (userid,))
 
 def rules(userid):
 	menu = SimpleMenu()
-	if is_queued(menu, indexFromUserid(userid)):
-		return  
 	menu.append(Text('The Rules'))
 	menu.append(Text(' '))
 	for i in get_rules:
 		menu.append(Text('%s' % (i)))
 	menu.append(Text(' '))
 	menu.append(Text('Do you accept the rules?'))
-	menu.append(Text(' '))    
-	menu.append(SimpleOption(8, 'No', 'No'))    
-	menu.append(SimpleOption(9, 'Yes', None))   
+	menu.append(Text(' '))
+	menu.append(SimpleOption(8, 'No', 'No'))
+	menu.append(SimpleOption(9, 'Yes', 'Yes'))
 	menu.select_callback = rule_menu_callback
-	menu.send(indexFromUserid(userid))          
-    
- 	 	    
+	menu.send(index_from_userid(userid))
+	
+
+def rule_menu_callback(_menu, _index, _option):
+	choice = _option.value
+	if choice:
+		userid = userid_from_index(_index)        
+		if choice == 'No':
+			queue_command_string('kickid %s You have to accept the rules!' % (userid))
+		elif choice == 'Yes':
+			SayText2('\x04Thank you for accepting the rules and have fun!').send(_index)
+     	    
